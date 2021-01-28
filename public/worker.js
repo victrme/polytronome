6281775cec0f2b96512cd3cbe6c1cabcb66bf7eb
Victor Azevedo <34415964+victorazevedo-me@users.noreply.github.com>
2021-01-28 18:18:06 +0100

    :truck: Moved Worker temporarly

--- public/worker.js
+++ public/worker.js
@@ -10,3 +10,28 @@
 // onmessage = function (e) {
 //     recursivetimeout(e.data)
 // }
+
+
+
+
+
+
+
+
+
+// let monworker: Worker
+
+	// function changeWorkerTest(which: 'start' | 'stop') {
+	// 	if (which === 'start') {
+	// 		monworker = new Worker(`${process.env.PUBLIC_URL}/worker.js`)
+	// 		monworker.postMessage(calculateTempoMs(4, metronome.tempo))
+
+	// 		monworker.onmessage = function (e) {
+	// 			console.log('timeoutID: ', e.data)
+	// 		}
+	// 	}
+
+	// 	if (which === 'stop' && monworker !== undefined) {
+	// 		monworker.terminate()
+	// 	}
+	// }
