e22b4724cbc985f3b7c86f5203548c5aa0c4bf43
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2021-10-15 10:34:51 +0200

    :bug: Catching up with master

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,8 +1,12 @@
 import Themes from '../assets/themes.json'
 import propTypes from 'prop-types'
 import Button from './Button'
+import { useEffect, useState } from 'react'
 
 const Menu = ({ moreSettings, setMoreSettings, easy, setEasy, menuShown, menuHovered }) => {
+	const [openedTheme, setOpenedTheme] = useState(false)
+	const [fullscreen, setFullscreen] = useState(false)
+
 	const changeAnimations = () => {
 		const appDOM = document.querySelector('.polytronome') as HTMLDivElement
 
@@ -19,29 +23,29 @@ const Menu = ({ moreSettings, setMoreSettings, easy, setEasy, menuShown, menuHov
 	const changeFullscreen = () => {
 		if (!moreSettings.fullscreen && document.fullscreenElement === null) {
 			document.body!.requestFullscreen()
+			setFullscreen(true)
 		} else if (document.fullscreenElement !== null) {
 			document.exitFullscreen()
+			setFullscreen(false)
 		}
-
-		setMoreSettings(prev => ({
-			...prev,
-			fullscreen: moreSettings.fullscreen,
-		}))
 	}
 
-	const changeTheme = () => {
-		const selected = (moreSettings.theme + 1) % Themes.length
+	const applyTheme = (index: number) => {
 		const root = document.querySelector(':root')! as HTMLBodyElement
-
-		Object.entries(Themes[selected]).forEach(([key, val]) =>
+		Object.entries(Themes[index]).forEach(([key, val]) =>
 			val !== undefined ? root.style.setProperty('--' + key, val) : ''
 		)
-
-		setMoreSettings(prev => ({ ...prev, theme: selected }))
-		localStorage.theme = selected
 	}
 
-	// const overlayRef = useRef(document.createElement('div'))
+	const changeTheme = (index: number) => {
+		applyTheme(index)
+		setMoreSettings(prev => ({ ...prev, theme: index }))
+		localStorage.theme = index
+	}
+
+	useEffect(() => {
+		applyTheme(moreSettings.theme)
+	}, [])
 
 	return (
 		<div className={'menu' + (menuShown ? ' shown' : menuHovered ? ' hovered' : '')}>
@@ -60,16 +64,39 @@ const Menu = ({ moreSettings, setMoreSettings, easy, setEasy, menuShown, menuHov
 					func={changeAnimations}
 				></Button>
 
+				<Button name="fullscreen" on={fullscreen} func={changeFullscreen}></Button>
+
 				<Button
-					name="fullscreen"
-					on={moreSettings.fullscreen}
-					func={changeFullscreen}
+					name="themes"
+					on={openedTheme}
+					func={e => setOpenedTheme(!openedTheme)}
 				></Button>
 
-				<Button name="theme" on={true} func={changeTheme}></Button>
-
-				<div className="profile">
-					<p>profile</p>
+				<div
+					className="theme-list"
+					style={{
+						maxHeight: openedTheme ? 90 : 0,
+						transition: 'max-height .5s',
+						overflow: 'hidden',
+					}}
+				>
+					{Themes.map((theme, i) => (
+						<span
+							key={i}
+							style={{
+								backgroundColor: theme.background,
+								color: theme.accent,
+								// borderTopLeftRadius: moreSettings.theme === i ? 0 : 20,
+							}}
+							onClick={e => {
+								e.stopPropagation()
+								e.nativeEvent.stopImmediatePropagation()
+								changeTheme(i)
+							}}
+						>
+							{theme.name}
+						</span>
+					))}
 				</div>
 
 				<p className="credit">
