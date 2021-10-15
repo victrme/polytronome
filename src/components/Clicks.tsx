9bf9d88db6cd3975f361b6fda0c2b2a0234b9c72
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2021-10-15 09:19:37 +0200

    :bug: Fixed clicks wrap duplicate, minor style changes

--- src/components/Clicks.tsx
+++ src/components/Clicks.tsx
@@ -1,5 +1,5 @@
-import propTypes from 'prop-types'
 import { useEffect, useRef, useState, useCallback } from 'react'
+import propTypes from 'prop-types'
 import Pizzicato from 'pizzicato'
 
 const Clicks = ({
@@ -194,19 +194,17 @@ const Clicks = ({
 	switch (lateSegmentChange) {
 		case true:
 			clicks = (
-				<div ref={clicksRef} className="clicks">
-					<div className="segment">
-						<div className="click-row">
-							{segment.ratios.map((ratio, i) => (
-								<span
-									key={i}
-									className={'click' + (segment.count === i ? ' on' : '')}
-									style={{
-										width: `calc(${ratio * 100}% - 10px)`,
-									}}
-								/>
-							))}
-						</div>
+				<div className="segment">
+					<div className="click-row">
+						{segment.ratios.map((ratio, i) => (
+							<span
+								key={i}
+								className={'click' + (segment.count === i ? ' on' : '')}
+								style={{
+									width: `calc(${ratio * 100}% - 10px)`,
+								}}
+							/>
+						))}
 					</div>
 				</div>
 			)
@@ -214,39 +212,37 @@ const Clicks = ({
 
 		case false: {
 			clicks = (
-				<div ref={clicksRef} className="clicks">
-					<div className="layers">
-						{layers.map((layer, row) => {
-							// Add clicks for each layers
+				<div className="layers">
+					{layers.map((layer, row) => {
+						// Add clicks for each layers
 
-							const children: JSX.Element[] = []
-							for (let beat = 0; beat < 16; beat++) {
-								children.push(
-									<div
-										key={beat}
-										className={
-											'click' +
-											(beat >= layer.beats
-												? ' off'
-												: beat < times[row]
-												? ' on'
-												: '')
-										}
-									/>
-								)
-							}
-
-							// Wrap in rows & return
-							return (
+						const children: JSX.Element[] = []
+						for (let beat = 0; beat < 16; beat++) {
+							children.push(
 								<div
-									key={row}
-									className={'click-row' + (layer.beats === 1 ? ' off' : '')}
-								>
-									{children}
-								</div>
+									key={beat}
+									className={
+										'click' +
+										(beat >= layer.beats
+											? ' off'
+											: beat < times[row]
+											? ' on'
+											: '')
+									}
+								/>
 							)
-						})}
-					</div>
+						}
+
+						// Wrap in rows & return
+						return (
+							<div
+								key={row}
+								className={'click-row' + (layer.beats === 1 ? ' off' : '')}
+							>
+								{children}
+							</div>
+						)
+					})}
 				</div>
 			)
 			break
