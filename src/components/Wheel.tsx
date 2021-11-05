83dc142e7ea5c579af75fc935175c834bb8224f4
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2021-11-05 17:42:02 +0100

    :hammer: Remade scroll event to wheels
    TODO: bounds & rubberbands ?

--- src/components/Wheel.tsx
+++ src/components/Wheel.tsx
@@ -1,6 +1,7 @@
-import { useDrag } from '@use-gesture/react'
+import { useDrag, useWheel } from '@use-gesture/react'
 import { useSpring, animated, config } from '@react-spring/web'
 import propTypes from 'prop-types'
+import { inRange } from 'lodash'
 
 const freqArr = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
@@ -18,22 +19,28 @@ const allLists = {
 	freq: fillArray(0, freqArr.length * 3, true),
 }
 
-allLists.beats.push('×')
+allLists.beats[allLists.beats.length - 1] = '×'
 
 function Wheel({ update, type, state }): JSX.Element {
 	const list: string[] = allLists[type]
 	const bottomPos = -(list.length - 1) * 50
 	const initialPos = bottomPos + (state - 1) * 50
 
-	const [{ x, y }, api] = useSpring(() => ({ x: 0, y: initialPos, config: config.gentle }))
+	const getClosest = (y: number) => Math.round(y / 50) * 50
+	const getUserVal = (y: number) => Math.round(y / 50) + list.length - 1
 
-	const bind = useDrag(
+	const [{ x, y }, api] = useSpring(() => ({
+		x: 0,
+		y: initialPos,
+		config: config.stiff,
+	}))
+
+	const dragging = useDrag(
 		({ active, offset: [x, y] }) => {
 			api.start({ y })
 			if (!active) {
-				api.start({ x, y: Math.round(y / 50) * 50 })
-				update(Math.round(y / 50) + list.length - 1)
-				console.log(Math.round(y / 50) + list.length - 1)
+				api.start({ x, y: getClosest(y) })
+				update(getUserVal(y))
 			}
 		},
 		{
@@ -45,9 +52,20 @@ function Wheel({ update, type, state }): JSX.Element {
 		}
 	)
 
+	const wheeling = useWheel(({ wheeling, direction }) => {
+		if (wheeling) {
+			const snapped = getClosest(y.get() + 50 * direction[1])
+
+			if (inRange(snapped, bottomPos)) {
+				api.start({ y: snapped })
+				update(getUserVal(snapped))
+			}
+		}
+	})
+
 	return (
 		<div className="immovable_wheel">
-			<animated.div {...bind()} className="wheel" style={{ y }}>
+			<animated.div {...dragging()} {...wheeling()} className="wheel" style={{ y }}>
 				<pre>{list.join('\n')}</pre>
 			</animated.div>
 		</div>
