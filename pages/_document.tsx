115bae66d603945bd9f8f6bbfaf0e00b337cf401
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2022-03-01 19:38:34 +0100

    ⚡️ Added next font optimization

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,12 @@ export default class MyDocument extends Document {
 	render() {
 		return (
 			<Html lang="en">
-				<Head />
+				<Head>
+					<link
+						href="https://fonts.googleapis.com/css2?family=Dosis:wght@600;700&family=Source+Code+Pro:wght@600&display=swap"
+						rel="stylesheet"
+					/>
+				</Head>
 				<body>
 					<Main />
 					<NextScript />
