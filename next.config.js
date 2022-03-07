754263e54f9103de9104b2e0e62a6a42a20c38bd
Victor Azevedo <victor.azevedo@mail.com>
2022-03-07 09:44:37 +0100

    ✨ Added next PWA

--- next.config.js
+++ next.config.js
@@ -2,7 +2,16 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 	enabled: process.env.ANALYZE === 'true',
 })
 
-module.exports = withBundleAnalyzer({
-	reactStrictMode: true,
-	basePath: '',
-})
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+
+module.exports = withPWA(
+	withBundleAnalyzer({
+		pwa: {
+			dest: 'public',
+			runtimeCaching,
+		},
+		reactStrictMode: true,
+		basePath: '',
+	})
+)
