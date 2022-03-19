fcdd9a6b0a5fa0951ad5e6e6589619f40d27988d
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2022-03-19 19:03:59 +0100

    📈 Fixed google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default class MyDocument extends Document {
 	render() {
@@ -9,6 +10,20 @@ export default class MyDocument extends Document {
 						href="https://fonts.googleapis.com/css2?family=Dosis:wght@600;700&family=Source+Code+Pro:wght@600&display=swap"
 						rel="stylesheet"
 					/>
+					<script
+						async
+						src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+					></script>
+					<script
+						dangerouslySetInnerHTML={{
+							__html: `
+							window.dataLayer = window.dataLayer || [];
+							function gtag(){dataLayer.push(arguments);}
+							gtag('js', new Date());
+							gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}'});
+						`,
+						}}
+					></script>
 				</Head>
 				<body>
 					<Main />
